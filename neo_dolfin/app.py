10d0124761e594b1213380d0c77d0e11868ee9b5
ShaneD <shane.dzartov@bigpond.com>
2023-08-22 15:50:16 +1000

    Update app.py

--- neo_dolfin/app.py
+++ neo_dolfin/app.py
@@ -16,7 +16,7 @@ import qrcode
 import logging 
 
 #import dash
-#import dash_core_components as dcc
+#import dash_core_components as dcc#
 #import dash_html_components as html
 
 load_dotenv()  # Load environment variables from .env
