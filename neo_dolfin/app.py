d71e2b078593805bb2406ce800792b848f1951a3
aditya-rajgor <adityarajgor88@gmail.com>
2023-08-28 21:43:11 +1000

    Created the Terms of Use page from the formal page. Added the bootstrap design and verified with Blake before pushing.

--- neo_dolfin/app.py
+++ neo_dolfin/app.py
@@ -291,6 +291,12 @@ def auth_FAQ():
         return redirect('/signin')  # Redirect to sign-in page if the token is expired
     if is_token_valid():
         return render_template("FAQ.html")
+    
+# APPLICATION TERMS OF USE PAGE - DOES NOT REQIRE AUTHENTICATION
+@app.route('/terms-of-use/')
+def open_terms_of_use():
+    # does not require the authentication to read this page
+    return render_template("TermsofUse.html") 
 
 # Define a Flask route for the Dash app's page
 #@app.route('/dash/')
