71d6d23857418ea13b880e0e539c1a937cc085cb
ShaneD <shane.dzartov@bigpond.com>
2023-08-31 17:15:50 +1000

    Added route testing.

--- neo_dolfin/test/test_app.py
+++ neo_dolfin/test/test_app.py
@@ -1,9 +1,8 @@
 # tests/test_app.py#
 from app import app
 
-def test_hello():
+def test_app_runs():
     client = app.test_client()
-
     response = client.get('/')
     assert response.status_code == 200
     assert "DolFin | Landing" in response.data.decode('utf-8')
