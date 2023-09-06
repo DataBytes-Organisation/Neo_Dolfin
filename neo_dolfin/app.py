e03bef17a0f6f5b8f589ff6416aeead949461852
Blake Latch <blatchie460@gmail.com>
2023-09-06 23:56:26 +1000

    Finished page and made scalable

--- neo_dolfin/app.py
+++ neo_dolfin/app.py
@@ -292,6 +292,14 @@ def auth_FAQ():
         return redirect('/signin')  # Redirect to sign-in page if the token is expired
     if is_token_valid():
         return render_template("FAQ.html")
+    
+# APPLICATION ARTICLE TEMPLATE PAGE - REQUIRES USER TO BE SIGNED IN TO ACCESS
+@app.route('/articleTemplate/')
+def auth_articleTemplate(): 
+    if not is_token_valid():
+        return redirect('/signin')  # Redirect to sign-in page if the token is expired
+    if is_token_valid():
+        return render_template("articleTemplate.html")
 
 # Define a Flask route for the Dash app's page
 #@app.route('/dash/')
