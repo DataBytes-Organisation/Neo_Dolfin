f1060a05a3196115f61ef5e5558ee611b1101346
souha1418 <127913869+souha1418@users.noreply.github.com>
2023-09-13 10:46:10 +1000

    Dashboard changes

--- neo_dolfin/app.py
+++ neo_dolfin/app.py
@@ -314,6 +314,14 @@ def auth_home():
     if is_token_valid():
         return render_template("home.html")
 
+@app.route('/dash/')
+def auth_dash(): 
+    #if not is_token_valid():
+     #   return redirect('/signin')  # Redirect to sign-in page if the token is expired
+    #if is_token_valid():
+    return render_template("dash.html")
+
+
 ## APPLICATION NEWS PAGE - REQUIRES USER TO BE SIGNED IN TO ACCESS    
 @app.route('/news/')
 def auth_news(): 
