103c8e615dfdcfacd2fea6296e69980658215ace
MoreDread <43129046+SDzartov@users.noreply.github.com>
2023-11-23 00:23:31 -0800

    Added initial page structure to project (#97)

--- dolfin_analytica/app.py
+++ dolfin_analytica/app.py
@@ -1,10 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+import time
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, Flask!'
+@app.route('/', methods=['GET', 'POST'])
+def landing():
+    if request.method == 'POST':
+        # Access form data using request.form
+        username = request.form['username']
+        password = request.form['password']
+
+        if username == 'dsuser':
+            if password == 'dolfin123':
+
+        # After processing the data, you can redirect to the /loading route
+                return redirect(url_for('loading'))
+
+    return render_template('landing.html')
+
+@app.route('/loading')
+def loading():
+    return render_template('loading.html')
+
+@app.route('/main')
+def main():
+    return render_template('main.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
