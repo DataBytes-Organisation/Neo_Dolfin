4cf46cf486dff70d7d22007d97334261fb4de545
washyking <maiblefive201@gmail.com>
2023-12-03 16:51:13 +0800

    analytica login page

--- dolfin_analytica/app.py
+++ dolfin_analytica/app.py
@@ -18,6 +18,42 @@ def landing():
 
     return render_template('landing.html')
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+
+        input_username = request.form['username']
+        input_password = request.form['password']
+
+        # Retrieve the user from the database
+        user = User.query.filter_by(username=input_username).first()
+
+        # Check if the user exists and the password is correct with stored hash
+        if user and bcrypt.checkpw(input_password.encode('utf-8'), user.password):
+            # Successful login, set a session variable to indicate that the user is logged in
+            session['user_id'] = user.username 
+
+            # If successful, check if test user or real user.
+            row = UserTestMap.query.filter_by(userid = input_username).first()
+            testId = 0
+            if row != None:
+                testId = row.testid
+                print('######### test id:', testId)
+
+            # Load transactional data
+            loadDatabase(testId)            
+            # log successful authentication challenge 
+            add_user_audit_log(input_username, 'login-success', 'User logged in successfully.')
+            # redirect to the dashboard.
+            return redirect('/dash')
+        
+        ## Otherwise:
+        # log un-successful authentication challenge
+        add_user_audit_log(input_username, 'login-fail', 'User login failed.')
+        return 'Login failed. Please check your credentials.'
+
+    return render_template('login.html')  # Create a login form in the HTML template
+
 @app.route('/loading')
 def loading():
     return render_template('loading.html')
