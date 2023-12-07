53563c431e743d9f294fdb336b40f9ae6624df50
GayatrideviPakki <114803017+GayatrideviPakki@users.noreply.github.com>
2023-12-07 14:57:37 +1100

    restored survey.html and survey.js created for testing

--- neo_dolfin/static/js/survey.js
+++ neo_dolfin/static/js/survey.js
@@ -1,7 +1,4 @@
-responses=[]
-
 function showHideTextArea(radioButtonId, containerId) {
-    responses.push({radioButtonId:radioButtonId});
     var noRadioButton = document.getElementById(radioButtonId);
     var textAreaContainer = document.getElementById(containerId);
 
@@ -12,9 +9,7 @@ function showHideTextArea(radioButtonId, containerId) {
     }
 }
 
-
 function changeColor(element, questionId, satisfactionLevel) {
-    
     console.log('Question ID: ' + questionId);
     console.log('Selected satisfaction level: ' + satisfactionLevel);
 
@@ -25,50 +20,18 @@ function changeColor(element, questionId, satisfactionLevel) {
 
     element.style.backgroundColor = 'rgba(225, 225, 225, 0.5)';
 }
-var wordCountElement = document.getElementById(wordCountId);
 
 function countWords(textboxClass, wordCountId) {
     var textarea = document.querySelector('.' + textboxClass);
     var wordCountElement = document.getElementById(wordCountId);
     var text = textarea.value.trim();
-    
     var words = text.split(/\s+/);
-    
 
     if (words.length > 100) {
         var truncatedText = words.slice(0, 100).join(' ');
         textarea.value = truncatedText;
         words = truncatedText.split(/\s+/);
     }
-    
+
     wordCountElement.textContent = words.length + ' words';
-    
-}
-
-
-
-
-function submitsurvey() {
-    
-    
-    console.log(responses);
-    
-    fetch('/submit', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-       
-        body: JSON.stringify(responses)
-    }).then(function (response) {
-        if (response.ok) {
-            return response.json();
-        } else {
-            return Promise.reject(new Error('Failed to load'));
-        }
-    }).then(function (data) {
-        console.log(data);
-    }).catch(function (error) {
-        console.log('Error: ', error);
-    });
 }
